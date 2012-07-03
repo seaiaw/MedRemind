e17d449dabf717cb4fe1622f487761c8de703aff
seaiaw <seaiaw.ang@2359media.com>
2012-07-03 17:37:01 +0800

    camera intent experiment

--- src/com/medremind/sg/MainActivity.java
+++ src/com/medremind/sg/MainActivity.java
@@ -1,17 +1,60 @@
 package com.medremind.sg;
 
-import android.os.Bundle;
+import java.io.File;
+
+import com.midnight.lockii.terminal.R;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private Uri fileUri;
+	private static final int CAPTURE_IMAGE = 100;
+	private Button cameraButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        cameraButton = (Button) findViewById(R.id.camera_button);
+        
+        cameraButton.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		 	Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        	        
+        	        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        	        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        	        
+        	        startActivityForResult(cameraIntent, CAPTURE_IMAGE);
+        	}
+        });
+        
+       
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == CAPTURE_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Toast.makeText(this, "Image saved to:\n" + data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }
     }
 
     @Override
@@ -20,5 +63,9 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    private void dispatchCameraIntent(int actionCode){
+    	Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    	startActivityForResult(cameraIntent, actionCode);
+    }
     
 }
